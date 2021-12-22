f46727fdcf7419b7912963e29a1165d06a3a5783
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2021-12-22 18:18:43 +0000

    complete

--- main.js
+++ main.js
@@ -4,24 +4,25 @@ The pizzas have arrived and some of them have pineapple on them. You need to fin
 of removing the pineapple from the array of pizza toppings.  
 
 For example:
-* Given[ham, cheese, tomato sauce, pineapple] your solution should return - pineapple
+* Given[ham, cheese, tomato sauce, pineapple] your solution should return - [ham, cheese, tomato sauce]
+if the ingredients does not include pineapple. you should return all ingredients 
 */
 
 //👉 
 
 
-const ingredients =['ham', 'cheese', 'tomato sauce', 'pineapple']
-// ['cheese', 'olives', 'peppers', 'basil']
-// ['tomato sauce', 'chicken', 'pineapple', 'sweetcorn', 'salami']
+//const ingredients =['ham', 'cheese', 'tomato sauce', 'pineapple']
+//['cheese', 'olives', 'peppers', 'basil']
+//['tomato sauce', 'chicken', 'pineapple', 'sweetcorn', 'salami']
 // ['cheese', 'tomato sauce', 'prawns', 'pineapple', 'squid', 'tuna']
 // ['cheese', 'tomato sauce']
 // ['cheese', 'tomato sauce', 'pepperoni', 'pineapple', 'beef', 'ham', 'salami', 'chicken']
 // ['cheese', 'tomato sauce', 'spicy beef', 'jalapeno', 'chili flakes']
 
-// ingredients.find(ewwPineapple)
-
-export default function ewwPineapple(ingredient){
-    return ingredients.slice('pineapple')
-}
+//ingredients.find(ewwPineapple)
 
 
+export default function ewwPineapple(ingredients){
+let found = 'pineapple'
+const newIngredient= ingredients.filter(item => item !== found)
+return newIngredient}
