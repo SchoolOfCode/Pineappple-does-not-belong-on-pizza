b1ce54a3cecd0098b0e35ffe68283fb19bc2d276
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2021-12-23 14:03:07 +0000

    Ready

--- main.js
+++ main.js
@@ -1,28 +1,14 @@
 /* 
 👉 It's a tough day for an office of hungry programmers. 
 The pizzas have arrived and some of them have pineapple on them. You need to find a way
-of removing the pineapple from the array of pizza toppings.  
+of removing the pineapple from the array of pizza toppings and return all other ingredients in an array..  
 
 For example:
 * Given[ham, cheese, tomato sauce, pineapple] your solution should return - [ham, cheese, tomato sauce]
-if the ingredients does not include pineapple. you should return all ingredients 
-*/
+if the ingredients does not include pineapple. you should return the original array.
+*/ 
 
-//👉 
-
-
-//const ingredients =['ham', 'cheese', 'tomato sauce', 'pineapple']
-//['cheese', 'olives', 'peppers', 'basil']
-//['tomato sauce', 'chicken', 'pineapple', 'sweetcorn', 'salami']
-// ['cheese', 'tomato sauce', 'prawns', 'pineapple', 'squid', 'tuna']
-// ['cheese', 'tomato sauce']
-// ['cheese', 'tomato sauce', 'pepperoni', 'pineapple', 'beef', 'ham', 'salami', 'chicken']
-// ['cheese', 'tomato sauce', 'spicy beef', 'jalapeno', 'chili flakes']
-
-//ingredients.find(ewwPineapple)
-
-
-export default function ewwPineapple(ingredients){
-let found = 'pineapple'
-const newIngredient= ingredients.filter(item => item !== found)
-return newIngredient}
+export default function ewwPineapple(ingredients)
+{
+return
+}
--- testplan.md
+++ testplan.md
@@ -3,5 +3,5 @@
 ['tomato sauce', 'chicken', 'pineapple', 'sweetcorn', 'salami']
 ['cheese', 'tomato sauce', 'prawns', 'pineapple', 'squid', 'tuna',]
 ['cheese', 'tomato sauce']
-['cheese', 'tomato sauce', 'pepperoni', 'pineapple', beef, 'ham', 'salami', 'chicken']
+['cheese', 'tomato sauce', 'pepperoni', 'pineapple', 'beef', 'ham', 'salami', 'chicken']
 ['cheese', 'tomato sauce', 'spicy beef', 'jalapeno', 'chili flakes']
